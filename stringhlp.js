b80937ec5dfbe170ea5d53cf984e09588a83f19a
Cyrp48 <162711318+Cyrp48@users.noreply.github.com>
2024-10-01 00:11:49 +0300

    Update stringhlp.js

--- stringhlp.js
+++ stringhlp.js
@@ -1,6 +1,5 @@
 // stringhlp.js
-
-const { concatenate, toUpperCase, toLowerCase, trimSpaces } = require('./stringFunctions');
+const { concatenate, toUpperCase, toLowerCase, trimSpaces, isEmail } = require('./stringFunctions');
 
 function formatString(str) {
     return toUpperCase(trimSpaces(str));
@@ -10,7 +9,12 @@ function combineStrings(str1, str2) {
     return concatenate(trimSpaces(str1), trimSpaces(str2));
 }
 
+function checkEmail(str) {
+    return isEmail(str) ? `${str} is a valid email.` : `${str} is not a valid email.`;
+}
+
 module.exports = {
     formatString,
     combineStrings,
+    checkEmail,
 };
